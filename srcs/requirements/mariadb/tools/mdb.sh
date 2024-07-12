121653067268b419b329d8475fb9dd7ddb7bbb90
amajid <abdelilahmajid012@gmail.com>
2024-07-12 19:00:41 +0100

    init

--- srcs/requirements/mariadb/tools/mdb.sh
+++ srcs/requirements/mariadb/tools/mdb.sh
@@ -1,16 +1,16 @@
 systemctl start mariadb.service
-sleep 7
+# sleep 7
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS '${MYSQL_DB}';"
 
-mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'localhost';"
+# mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'localhost';"
 
-mariadb -e "FLUSH PRIVILEGES;"
+# mariadb -e "FLUSH PRIVILEGES;"
 
 #shut down mariadb
-systemctl stop mariadb
+# systemctl stop mariadb
 
 #start mariadb
-mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+# mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
