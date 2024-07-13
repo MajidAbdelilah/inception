57518969e324f26da45607db56a7d62d1a8d8994
amajid <abdelilahmajid012@gmail.com>
2024-07-13 23:29:37 +0100

    init

--- srcs/requirements/mariadb/tools/mdb.sh
+++ srcs/requirements/mariadb/tools/mdb.sh
@@ -1,12 +1,12 @@
 set -x
 service mariadb start # start mariadb
-sleep 5 # wait for mariadb to start
-
-# Create database if not exists
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+sleep 8 # wait for mariadb to start
 
 # Create user if not exists
 mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+# Create database if not exists
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
 
 # Grant privileges to user
 mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
